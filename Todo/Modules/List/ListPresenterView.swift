8d8a59b05d691cec618145713740829374ed745f
Mohd Nafish <83894846+iosWithNafish@users.noreply.github.com>
2026-03-11 20:14:52 +0530

    Fizxed a bug

--- Todo/Modules/List/ListPresenterView.swift
+++ Todo/Modules/List/ListPresenterView.swift
@@ -10,18 +10,19 @@ import SwiftData
 import Playgrounds
 
 struct ListPresenterView: View {
-	@State var interactor: ListInteractor
-	@State var wireframe: ListWireframe
+    @State var interactor: ListInteractor
+    @State var wireframe: ListWireframe
 
-	var body: some View {
-		ListContentView(
-			todayItems: interactor.todayItems,
-			nextWeekItems: interactor.nextWeekItems,
-			addAction: { wireframe.presentAddInterface() }
-		)
-		.onAppear { interactor.findUpcomingItems() }
-		.onChange(of: wireframe.isAddInterfacePresented) {
-			interactor.findUpcomingItems()
-		}
-	}
+    var body: some View {
+        ListContentView(
+            todayItems: interactor.todayItems,
+            Upcoming: interactor.upcomingItems,
+            nextWeekItems: interactor.nextWeekItems,
+            addAction: { wireframe.presentAddInterface() }
+        )
+        .onAppear { interactor.findUpcomingItems() }
+        .onChange(of: wireframe.isAddInterfacePresented) {
+            interactor.findUpcomingItems()
+        }
+    }
 }
